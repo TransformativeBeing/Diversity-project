f28e941ed919c3ba688bc38acb85c1f35b9d10de
TransformativeBeing <83785114+TransformativeBeing@users.noreply.github.com>
2021-07-24 15:33:36 -0700

    Some fine tuning
    Simplified app.js, cleaned up index.html, adjusted font sizes style.css

--- src/app.js
+++ src/app.js
@@ -1,11 +1,8 @@
-let discoverButton = document.querySelector("button");
-discoverButton.addEventListener("click", discover);
-
-function discover() {
+document.querySelector("button").addEventListener("click", function() {
     let name = prompt("What what is your name?");
     let email = prompt("What is your email?");
     let emoji = prompt("What is your favorite emoji");
     alert(
         ` ${emoji} Thank you, ${name}! Check your email for your NeroMap Quiz! `
     );
-}
+});
